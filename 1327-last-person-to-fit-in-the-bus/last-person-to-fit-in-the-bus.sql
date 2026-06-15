d5f68d15209418bf131d80e2a644b2af0c192987
Sameer Suman <76219383+sameer2403@users.noreply.github.com>
2026-06-15 11:45:25 +0530

    Time: 1031 ms (24.64%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,13 +1,12 @@
 # Write your MySQL query statement below
-with cte as (select turn,person_name,weight,
-    sum(weight) over(order by turn) as running_wt,
-    row_number()over(order by turn) as rn
-    from Queue),
-cte2 as(select     person_name, turn , running_wt,
-row_number() over(order by turn desc) as rn
-from cte
-where running_wt<=1000)
-select person_name
-from cte2
-where rn=1
-
+WITH cte AS (
+    SELECT 
+        person_name,
+        SUM(weight) OVER (ORDER BY turn) AS running_wt
+    FROM Queue
+)
+SELECT person_name
+FROM cte
+WHERE running_wt <= 1000
+ORDER BY running_wt DESC
+LIMIT 1;
